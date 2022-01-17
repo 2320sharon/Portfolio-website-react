4db6c3fc631bdcb1f3f19e5a3c24e2c666a2677c
Sharon Fitzpatrick <SF2309@Gmail.Com>
2022-01-16 17:42:47 -0800

    portfolio section basics done

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,11 +1,78 @@
 import React from 'react'
+import PortfolioList from '../portfolioList/PortfolioList'
 import './portfolio.scss'
+import {useEffect, useState} from "react";
 
-function portfolio() {
+function Portfolio() {
+    const [selected,Setselected]=useState("featured")
+    const list = [
+        {
+            id: "featured",
+            title: "Featured",
+          },
+          {
+            id: "web",
+            title: "Web App",
+          },
+          {
+            id: "mobile",
+            title: "Mobile App",
+          },
+          {
+            id: "design",
+            title: "Design",
+          },
+          {
+            id: "content",
+            title: "Content",
+          },
+    ]
+
     return (
-        <div className='portfolio'id='portfolio'>
+        <div className='portfolio'>
+            <h1>Portfolio</h1>
+                <ul>
+
+              {list.map(item=> (<PortfolioList title={item.title} active = {selected === item.id} Setselected={Setselected} id={item.id}/>))}
+                    
+                </ul>
+                <div className="container">
+                    <div className="item">
+                        <img src="https://i.pinimg.com/564x/22/9b/ca/229bca86ffd643ed7e5669fdff422ee6.jpg" alt="" />
+                        <h3>Banking App</h3>
+                    </div>
+                    <div className="item">
+                        <img src="https://i.pinimg.com/564x/22/9b/ca/229bca86ffd643ed7e5669fdff422ee6.jpg" alt="" />
+                        <h3>Banking App</h3>
+                    </div>
+                    <div className="item">
+                        <img src="https://i.pinimg.com/564x/22/9b/ca/229bca86ffd643ed7e5669fdff422ee6.jpg" alt="" />
+                        <h3>Banking App</h3>
+                    </div>
+                    <div className="item">
+                        <img src="https://i.pinimg.com/564x/22/9b/ca/229bca86ffd643ed7e5669fdff422ee6.jpg" alt="" />
+                        <h3>Banking App</h3>
+                    </div>
+                    <div className="item">
+                        <img src="https://i.pinimg.com/564x/22/9b/ca/229bca86ffd643ed7e5669fdff422ee6.jpg" alt="" />
+                        <h3>Banking App</h3>
+                    </div>
+                    <div className="item">
+                        <img src="https://i.pinimg.com/564x/22/9b/ca/229bca86ffd643ed7e5669fdff422ee6.jpg" alt="" />
+                        <h3>Banking App</h3>
+                    </div>
+                    <div className="item">
+                        <img src="https://i.pinimg.com/564x/22/9b/ca/229bca86ffd643ed7e5669fdff422ee6.jpg" alt="" />
+                        <h3>Banking App</h3>
+                    </div>
+                    <div className="item">
+                        <img src="https://i.pinimg.com/564x/22/9b/ca/229bca86ffd643ed7e5669fdff422ee6.jpg" alt="" />
+                        <h3>Banking App</h3>
+                    </div>
+                </div>
+            
         </div>
     )
 }
 
-export default portfolio
+export default Portfolio
