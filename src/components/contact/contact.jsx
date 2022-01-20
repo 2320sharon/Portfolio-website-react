f52af9cd1c0b1146632b64fd1906a25b797c0c56
Sharon Fitzpatrick <SF2309@Gmail.Com>
2022-01-19 23:08:14 -0800

    contact page finished

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,12 +1,32 @@
 import React from 'react'
+import { useState } from 'react';
 import "./contact.scss"
 
-function contact() {
+function Contact() {
+    const [message,setMessage]=useState(false);
+
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        setMessage(true);
+    }
+
     return (
         <div className='contact' id='contact'>
-            
+            <div className="left">
+                <img src="assets/shake.svg" alt="" />
+            </div>
+            <div className="right">
+                <h2>Contact.</h2>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" placeholder='Email'/>
+                    <textarea  placeholder='Message'></textarea>
+                    <button type="submit">Send</button>
+                    
+                    {message && <span>Thank you! I will reply soon!</span>}
+                </form>
+            </div>
         </div>
     )
 }
 
-export default contact
+export default Contact
