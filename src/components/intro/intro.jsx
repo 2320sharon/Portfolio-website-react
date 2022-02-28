25f37ae7ae56ad88f648c61a5b3180dfbe03c2d9
Sharon Fitzpatrick <SF2309@Gmail.Com>
2022-02-28 11:52:17 -0800

    responsiveness improvments

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -11,7 +11,12 @@ function Intro() {
       showCursor: true,
       backDelay: 1500,
       backSpeed: 60,
-      strings: ["Developer", "Data Analyst", "Blogger"],
+      strings: [
+        "Web Developer",
+        "Python Developer",
+        "Software Engineer",
+        "Blogger",
+      ],
     });
   }, []);
 
@@ -25,20 +30,23 @@ function Intro() {
         </div>
       </div>
       <div className="right">
-        <div className="wrapper">
-          <h2>Hi there, I'm</h2>
-          <h1>Sharon Fitzpatrick</h1>
-          <h3>
-            Freelance <span ref={textRef}></span>{" "}
-          </h3>
-        </div>
-        <div className="aboutme">
-          <p>
-            Hey there I specialize in web development in react and development
-            with Python.I've built cross-platform apps with React Native for UC
-            Davis and I'm currently building research applications using Python
-            for the USGS.
-          </p>
+        <div className="textContainer">
+          <div className="wrapper">
+            <h2>Hi there, I'm</h2>
+            <h1>Sharon Fitzpatrick</h1>
+            <h3>
+              Freelance <span ref={textRef}></span>{" "}
+            </h3>
+          </div>
+          <div className="aboutme">
+            <p>
+              Hey there! I recently graduated college with a 3.9 GPA where I
+              just finished developing a mobile app for UC Davis Health. I
+              specialize in web development with React and development with
+              Python. I'm currently building research applications using Python
+              for the USGS.
+            </p>
+          </div>
         </div>
         <a href="#portfolio">
           <img src="assets/down.png" alt="" />
