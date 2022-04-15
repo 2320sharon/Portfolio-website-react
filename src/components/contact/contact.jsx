1ee54723f13716f39606037aca46cb1f2121f5cf
Sharon Fitzpatrick <SF2309@Gmail.Com>
2022-04-15 16:07:15 -0700

    formspree adds email functionality contact page

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,32 +1,37 @@
-import React from 'react'
-import { useState } from 'react';
-import "./contact.scss"
+import React from "react";
+import { useForm, ValidationError } from "@formspree/react";
+import "./contact.scss";
 
 function Contact() {
-    const [message,setMessage]=useState(false);
-
-    const handleSubmit=(e)=>{
-        e.preventDefault();
-        setMessage(true);
-    }
-
-    return (
-        <div className='contact' id='contact'>
-            <div className="left">
-                <img src="assets/shake.svg" alt="" />
-            </div>
-            <div className="right">
-                <h2>Contact.</h2>
-                <form onSubmit={handleSubmit}>
-                    <input type="text" placeholder='Email'/>
-                    <textarea  placeholder='Message'></textarea>
-                    <button type="submit">Send</button>
-                    
-                    {message && <span>Thank you! I will reply soon!</span>}
-                </form>
-            </div>
-        </div>
-    )
+  const [state, handleSubmit] = useForm("meqnrrlj");
+  return (
+    <div className="contact" id="contact">
+      <div className="left">
+        <img src="assets/shake.svg" alt="" />
+      </div>
+      <div className="right">
+        <h2> Contact Me.</h2>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">
+            Feel free to email me about any freelance projects or job
+            opportunities.
+          </label>
+          <input id="email" type="email" name="email" placeholder="Email" />
+          <ValidationError prefix="Email" field="email" errors={state.errors} />
+          <textarea id="message" name="message" placeholder="Message" />
+          <ValidationError
+            prefix="Message"
+            field="message"
+            errors={state.errors}
+          />
+          <button type="submit" disabled={state.submitting}>
+            Submit
+          </button>
+          {state.succeeded && <span>Thank you! I will reply soon!</span>}
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
